5d5b2f912c712ed5c0237666b0ec8a3d726996ef
Vansh2011 <gupta20vansh@gmail.com>
2026-02-09 23:04:27 +0530

    adding another file along with modifications for tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl text-red-600 p-5 bg-pink-200">
+      <Text className="text-5xl text-center font-extrabold text-dark-200 p-5 bg-pink-200">
         Welcome to the movie show.
       </Text>
     </View>
