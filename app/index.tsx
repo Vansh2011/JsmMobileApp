01db2840c2d769ae02c879612008e22ea9a16ae8
Vansh2011 <gupta20vansh@gmail.com>
2026-02-09 22:34:11 +0530

    first file to commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl text-red-600 p-5 bg-pink-200">
+        Welcome to the movie show.
+      </Text>
+    </View>
+  );
+}
