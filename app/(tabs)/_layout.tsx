7805e2aece68c7dc19c653be1420f27b9c8cfc12
Vansh2011 <gupta20vansh@gmail.com>
2026-02-14 03:37:35 +0530

    complete navbar without animations and changing the logo and splash screen for the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,105 @@
+import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
-import { ImageBackground } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 
+const RenderTabFocusedIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images?.highlight}
+        className="flex flex-row w-full gap-2 flex-1 min-w-[112px] overflow-hidden min-h-16 mt-4 justify-center items-center rounded-full"
+      >
+        <Image source={icon} tintColor={"#151312"} className="size-5" />
+        <Text className="text-secondary text-base text-semibold">{title}</Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor={"#A8B5DB"} className="size-5" />
+    </View>
+  );
+};
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
-          tabBarIcon: ({ focused }) => {
-            <>
-              <ImageBackground source={images?.highlight}></ImageBackground>
-            </>;
-          },
+          tabBarIcon: ({ focused }) => (
+            <RenderTabFocusedIcon
+              focused={focused}
+              icon={icons?.home}
+              title={"Home"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="search"
-        options={{ title: "Search", headerShown: false }}
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <RenderTabFocusedIcon
+              focused={focused}
+              icon={icons?.search}
+              title={"Search"}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="saved"
-        options={{ title: "Saved", headerShown: false }}
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <RenderTabFocusedIcon
+              focused={focused}
+              icon={icons?.save}
+              title={"Saved"}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
-        options={{ title: "Profile", headerShown: false }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <RenderTabFocusedIcon
+              focused={focused}
+              icon={icons?.person}
+              title={"Profile"}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
