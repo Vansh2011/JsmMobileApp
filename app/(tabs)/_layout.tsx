f4ed894a05cb8ebf7576e887dc7a1f1a1da04f0f
Vansh2011 <gupta20vansh@gmail.com>
2026-02-14 02:59:46 +0530

    second with the routing of pages

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import { ImageBackground } from "react-native";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            <>
+              <ImageBackground source={images?.highlight}>
+                <Image />
+              </ImageBackground>
+            </>;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ title: "Search", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ title: "Saved", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: "Profile", headerShown: false }}
+      />
+    </Tabs>
+  );
+}
